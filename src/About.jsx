6354b63016c5e3515222e69d27f1fc9b4d6ee04b
Henrique Duarte Rosa <l51923@alunos.uevora.pt>
2024-10-12 18:10:37 +0100

    finished about page

--- src/About.jsx
+++ src/About.jsx
@@ -1,5 +1,6 @@
 import Transition from "./transition";
 import Timeline from "./timeline";
+import Timeline2 from "./Timeline2";
 import "./scrollbar.css";
 import "./about.css"
 import { motion} from "framer-motion";
@@ -36,34 +37,10 @@ function About() {
             I live in Aveiro, but I was born and raised in the arid lands of Alentejo. I am currently 
             still developing my knowledge, undertaking a masters degree in the University of Aveiro. 
             I love challenges, and I have a fervorous drive for being the best and excelling at everything 
-            I do, and I believe my main traits are my open-mindedness, my great socials skills and ability 
-            to work in a team environment, and my desire to excel.
+            I do, and I believe my main traits are my open-mindedness, my ease to learn new things, my great socials skills
+            and ability to work in a team environment, and my desire to excel.
           </motion.p>
 
-          <div className="header-container">
-            <motion.img src="./profile.svg"
-              initial={{opacity:0, y: 50}} 
-              whileInView={{opacity:1, y:0 ,transition:{delay:0.2, duration:0.3}}} 
-              viewport={{once:false, amount:.5}} className="about-img" />
-            <motion.h1 
-              className="about-h1"
-              initial={{opacity:0, y: 50}} 
-              whileInView={{opacity:1, y:0 ,transition:{delay:0.2, duration:0.3}}} 
-              viewport={{once:false, amount:.5}}> Who I am: </motion.h1>
-          </div>
-        
-          <motion.p
-          initial={{opacity:0, y: 50}} 
-          whileInView={{opacity:1, y:0 ,transition:{delay:0.2, duration:0.3}}} 
-          viewport={{once:false, amount:.5}}>
-            Hi! My name is Henrique, and I am a Portuguese Web Developer and Computer Sciences Engineer. 
-            I live in Aveiro, but I was born and raised in the arid lands of Alentejo. I am currently 
-            still developing my knowledge, undertaking a masters degree in the University of Aveiro. 
-            I love challenges, and I have a fervorous drive for being the best and excelling at everything 
-            I do, and I believe my main traits are my open-mindedness, my great socials skills and ability 
-            to work in a team environment, and my desire to excel.
-          </motion.p>
-          
           <div className="header-container">
             <motion.img src="./book.svg"
               initial={{opacity:0, y: 50}} 
@@ -78,7 +55,7 @@ function About() {
           <Timeline/>
 
           <div className="header-container">
-            <motion.img src="./profile.svg"
+            <motion.img src="./keyboard.svg"
               initial={{opacity:0, y: 50}} 
               whileInView={{opacity:1, y:0 ,transition:{delay:0.2, duration:0.3}}} 
               viewport={{once:false, amount:.5}} className="about-img" />
@@ -86,22 +63,118 @@ function About() {
               className="about-h1"
               initial={{opacity:0, y: 50}} 
               whileInView={{opacity:1, y:0 ,transition:{delay:0.2, duration:0.3}}} 
-              viewport={{once:false, amount:.5}}> Who I am: </motion.h1>
+              viewport={{once:false, amount:.5}}> Professional experience </motion.h1>
           </div>
         
+          <Timeline2></Timeline2>
+        
+          <div className="header-container">
+            <motion.img src="./convo.svg"
+              initial={{opacity:0, y: 50}} 
+              whileInView={{opacity:1, y:0 ,transition:{delay:0.2, duration:0.3}}} 
+              viewport={{once:false, amount:.5}} className="about-img" />
+            <motion.h1 
+              className="about-h1"
+              initial={{opacity:0, y: 50}} 
+              whileInView={{opacity:1, y:0 ,transition:{delay:0.2, duration:0.3}}} 
+              viewport={{once:false, amount:.5}}> Comunication Skills </motion.h1>
+          </div>
+
           <motion.p
           initial={{opacity:0, y: 50}} 
           whileInView={{opacity:1, y:0 ,transition:{delay:0.2, duration:0.3}}} 
           viewport={{once:false, amount:.5}}>
-            Hi! My name is Henrique, and I am a Portuguese Web Developer and Computer Sciences Engineer. 
-            I live in Aveiro, but I was born and raised in the arid lands of Alentejo. I am currently 
-            still developing my knowledge, undertaking a masters degree in the University of Aveiro. 
-            I love challenges, and I have a fervorous drive for being the best and excelling at everything 
-            I do, and I believe my main traits are my open-mindedness, my great socials skills and ability 
-            to work in a team environment, and my desire to excel.
+            I've always been an expressive person, capable of easily conveying ideas to others, as well as a good, impartial
+            and analytical listener. Whether I'm explaining complex concepts, 
+            offering feedback, or collaborating on projects, I have a natural ability to engage people and make my 
+            points clear and easy to understand. I also tend to tailor my communication style 
+            depending on the audience, which I believe shows my awareness and empathy in every interaction. It’s not just 
+            about the words I use, but also how I listen and respond thoughtfully, creating a comfortable and 
+            open environment for dialogue. I believe that these skills are a huge asset, especially on my line of work as an engineer,
+            designer and problem solver, where clear and effective communication is key to driving success and getting the
+            best outcome possible.
+            In addition, here are the languages I speak, and my proficiency in them:
+            <ul>
+              <li>Portuguese - Native</li>
+              <li>English - Fluent <a href="./english_cert.pdf" target="_blank">(certificate)</a></li>
+              <li>Spanish - Advanced</li>
+              <li>Italian - Begginer</li>
+            </ul>
           </motion.p>
-        
-          
+
+          <div className="header-container">
+            <motion.img src="./people.svg"
+              initial={{opacity:0, y: 50}} 
+              whileInView={{opacity:1, y:0 ,transition:{delay:0.2, duration:0.3}}} 
+              viewport={{once:false, amount:.5}} className="about-img" />
+            <motion.h1 
+              className="about-h1"
+              initial={{opacity:0, y: 50}} 
+              whileInView={{opacity:1, y:0 ,transition:{delay:0.2, duration:0.3}}} 
+              viewport={{once:false, amount:.5}}> Networks </motion.h1>
+          </div>
+
+          <motion.p
+          initial={{opacity:0, y: 50}} 
+          whileInView={{opacity:1, y:0 ,transition:{delay:0.2, duration:0.3}}} 
+          viewport={{once:false, amount:.5}}>
+          <ul>
+            <li>Academical asdsociation of the University of Évora | Vice-Coordinator of the IT sector </li>
+            <li>Computer Engineering Sudents Conglomerate | Vice-Coordinator of the IT department</li>
+          </ul>  
+          </motion.p>
+
+          <div className="header-container">
+            <motion.img src="./organization.svg"
+              initial={{opacity:0, y: 50}} 
+              whileInView={{opacity:1, y:0 ,transition:{delay:0.2, duration:0.3}}} 
+              viewport={{once:false, amount:.5}} className="about-img" />
+            <motion.h1 
+              className="about-h1"
+              initial={{opacity:0, y: 50}} 
+              whileInView={{opacity:1, y:0 ,transition:{delay:0.2, duration:0.3}}} 
+              viewport={{once:false, amount:.5}}> Organizational Skills </motion.h1>
+          </div>
+
+          <motion.p
+          initial={{opacity:0, y: 50}} 
+          whileInView={{opacity:1, y:0 ,transition:{delay:0.2, duration:0.3}}} 
+          viewport={{once:false, amount:.5}}>
+          Practice in effective and organized time management, paired with robust data management practices and 
+          secure data protocols, ensuring a structured and reliable workflow, maintaining at all times a clear 
+          vision for setting and tracking goals, along with a well-defined path to achieve them. 
+          Additionally, I approach tasks with a quick and analytical understanding, ensuring efficient 
+          execution and insightful problem-solving.
+          </motion.p>
+
+          <div className="header-container">
+            <motion.img src="./bulb.svg"
+              initial={{opacity:0, y: 50}} 
+              whileInView={{opacity:1, y:0 ,transition:{delay:0.2, duration:0.3}}} 
+              viewport={{once:false, amount:.5}} className="about-img" />
+            <motion.h1 
+              className="about-h1"
+              initial={{opacity:0, y: 50}} 
+              whileInView={{opacity:1, y:0 ,transition:{delay:0.2, duration:0.3}}} 
+              viewport={{once:false, amount:.5}}> Personal Interests </motion.h1>
+          </div>
+
+          <motion.p
+          initial={{opacity:0, y: 50}} 
+          whileInView={{opacity:1, y:0 ,transition:{delay:0.2, duration:0.3}}} 
+          viewport={{once:false, amount:.5}}>
+            I consider myself a multifaceted person, with lots of interests, like the following:
+          <ul>
+            <li>Programming</li>
+            <li>Drawing</li>
+            <li>Playing and writing music</li>
+            <li>Singing</li>
+            <li>Gaming</li>
+            <li>Reading</li>
+            <li>Calisthenics</li>
+          </ul>
+          </motion.p>
+
         </div>
       </div>
     </>
--- src/Timeline2.jsx
+++ src/Timeline2.jsx
@@ -1,7 +1,7 @@
 import "./timeline.css";
 import { motion} from "framer-motion";
 
-function timeline(){
+function timeline2(){
     return(<>
         <div className="timeline">
             <div className="events">
@@ -12,10 +12,12 @@ function timeline(){
                 className="event">
                     <svg className="marker" xmlns="http://www.w3.org/2000/svg" width="15" height="12"> <circle cx="6" cy="6" r="6"></circle> </svg>
                     <div className="content">
-                        <time>2018 - 2020</time>
+                        <time>01/07/2023 – 31/07/2023 </time>
                         <div className="text">
-                            <h2>High School Course of Science and Technology</h2>
-                            <p>Évora | <a href="https://aeseverim.pt/" target="_blank">Severim de Faria's High School</a> </p>
+                            <h2>Summer Internship</h2>
+                            <p>Integration on KPMG's Technology Consulting department, with work
+                            pertaining to the development of software solutions with OutSystems</p>
+                            <p>Évora | <a href="https://kpmg.com/pt/pt/home.html" target="_blank">KPMG</a> | <a href="./KPMG_estagio.pdf" target="_blank">Certificate of Completion</a></p>
                         </div>
                     </div>   
                 </motion.div>  
@@ -27,13 +29,19 @@ function timeline(){
                 className="event">
                     <svg className="marker" xmlns="http://www.w3.org/2000/svg" width="15" height="12"> <circle cx="6" cy="6" r="6"></circle> </svg>
                     <div className="content">
-                        <time>2021 – 2024</time>
+                        <time>06/03/2024 – 22/05/2024</time>
                         <div className="text">
-                            <h2>Bachelors Degree in Computer Science Engineering</h2>
-                            <p>Évora | <a href="https://www.uevora.pt/en" target="_blank">University of Évora </a> | <a href="./diploma_UE_EI.pdf" target="_blank">Certificate of Completion</a> </p>
+                            <h2>Curricular Intership</h2>
+                            <p>Curricular internship, belonging to the curriculum of my bachelors degree in
+                                Computer Science Engineering. Work in the Development of an AI solutions
+                                for the automation of business processes with the ServiceNow platform.
+                                Construction of documentation for the guided implementation of said
+                                solution</p>
+                            <p>Évora | <a href="https://kpmg.com/pt/pt/home.html" target="_blank">KPMG</a></p>
                         </div>
                     </div>   
                 </motion.div> 
+
                 <motion.div
                 initial={{opacity:0, y: 50}} 
                 whileInView={{opacity:1, y:0 ,transition:{delay:0.2, duration:0.3}}} 
@@ -43,15 +51,16 @@ function timeline(){
                     <div className="content">
                         <time>CURRENTLY</time>
                         <div className="text">
-                            <h2>Masters Degree in Comunication and Web Development</h2>
-                            <p>Aveiro | <a href="https://www.ua.pt/" target="_blank">University of Aveiro</a> | <a href="./Matricula_UA.pdf" target="_blank">Entry Certificate </a> </p>
+                            <h2>Looking for oportunities!</h2>
+                            
                         </div>
                     </div>   
                 </motion.div>
+                
                 
             </div>
         </div>
     </>)
 }
 
-export default timeline;
+export default timeline2;
